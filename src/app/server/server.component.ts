6549c462b204074adcf25046a64a0ace0683c721
Anil7789 <anilreddy0536@gmail.com>
2020-02-26 23:00:22 +0530

    main source

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-server',
+  // selector: '[app-server]',
+  // selector: '.app-server',
+  templateUrl: './server.component.html',
+  styleUrls: ['./server.component.scss']
+})
+export class ServerComponent implements OnInit {
+
+  serverId: number = 10;
+  serverStatus: string = 'offline';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  public getServerStatus(){
+    return this.serverStatus;
+  }
+
+}
